a459bb50d7139d554908b9a02c02cdcc8fba2842
Kevin Smith <kev_bite@msn.com>
2022-11-11 13:39:33 +0000

    Update MassTransit

--- src/Payments/Program.cs
+++ src/Payments/Program.cs
@@ -1,34 +1,26 @@
 ﻿using System;
 using System.Linq;
 using MassTransit;
+using Payments;
 
-namespace Payments
+var bus = Bus.Factory.CreateUsingRabbitMq(sbc =>
 {
-    class Program
+    sbc.Host(new Uri("rabbitmq://localhost"), h =>
     {
-        static void Main(string[] args)
-        {
-            var bus = Bus.Factory.CreateUsingRabbitMq(sbc =>
-            {
-                var host = sbc.Host(new Uri("rabbitmq://localhost"), h =>
-                {
-                    h.Username("guest");
-                    h.Password("guest");
-                });
+        h.Username("guest");
+        h.Password("guest");
+    });
 
-                sbc.ReceiveEndpoint(host, "Payments", ep =>
-                {
-                    ep.Consumer(() => new OrderRequestedConsumer());
-                });
-            });
+    sbc.ReceiveEndpoint("Payments", ep =>
+    {
+        ep.Consumer(() => new OrderRequestedConsumer());
+    });
+});
 
-            bus.Start();
-            
-            Console.WriteLine("Welcome to Payments");
-            Console.WriteLine("Press Q key to exit");
-            while (Console.ReadKey(true).Key != ConsoleKey.Q) ;
+bus.Start();
 
-            bus.Stop();
-        }
-    }
-}
+Console.WriteLine("Welcome to Payments");
+Console.WriteLine("Press Q key to exit");
+while (Console.ReadKey(true).Key != ConsoleKey.Q) ;
+
+bus.Stop();
--- src/Shop/Program.cs
+++ src/Shop/Program.cs
@@ -3,81 +3,73 @@ using System.Collections.Generic;
 using System.Linq;
 using MassTransit;
 using Messages;
+using Shop;
 
-namespace Shop
+IReadOnlyList<(string name, decimal price)> Products = new List<(string, decimal)>
 {
-    class Program
+    ("Bread", 1.20m),
+    ("Milk", 0.50m),
+    ("Rice", 1m),
+    ("Pasta", 0.9m),
+    ("Pasta", 0.9m),
+    ("Cereals", 1.6m),
+    ("Chocolate", 2m),
+    ("Noodles", 1m),
+    ("Pie", 1m),
+    ("Sandwich", 1m),
+};
+
+var bus = Bus.Factory.CreateUsingRabbitMq(sbc =>
+{
+    sbc.Host(new Uri("rabbitmq://localhost"), h =>
     {
-        static void Main(string[] args)
+        h.Username("guest");
+        h.Password("guest");
+    });
+
+    sbc.ReceiveEndpoint("Shop", ep =>
+    {
+        ep.Consumer(() => new OrderRequestedFaultConsumer());
+    });
+});
+
+bus.Start();
+
+Console.WriteLine("Welcome to the Shop");
+Console.WriteLine("Press Q key to exit");
+Console.WriteLine("Press [0..9] key to order some products");
+Console.WriteLine(string.Join(Environment.NewLine, Products.Select((x, i) => $"[{i}]: {x.name} @ {x.price:C}")));
+
+var products = new List<(string name, decimal price)>();
+for (;;)
+{
+    var consoleKeyInfo = Console.ReadKey(true);
+    if (consoleKeyInfo.Key == ConsoleKey.Q)
+    {
+        break;
+    }
+
+
+    if (char.IsNumber(consoleKeyInfo.KeyChar))
+    {
+        // Hack: I don't care about ½ etc...
+        var product = Products[(int)char.GetNumericValue(consoleKeyInfo.KeyChar)];
+        products.Add(product);
+        Console.WriteLine($"Added {product.name}");
+    }
+
+    if (consoleKeyInfo.Key == ConsoleKey.Enter)
+    {
+        bus.Publish<IOrderRequested>(new
         {
-            var bus = Bus.Factory.CreateUsingRabbitMq(sbc =>
-            {
-                var host = sbc.Host(new Uri("rabbitmq://localhost"), h =>
-                {
-                    h.Username("guest");
-                    h.Password("guest");
-                });
+            Products = products.Select(x => new { Name = x.name, Price = x.price }).ToList()
+        });
 
-                sbc.ReceiveEndpoint(host, "Shop", ep =>
-                {
-                    ep.Consumer(() => new OrderRequestedFaultConsumer());
-                });
-            });
+        Console.WriteLine("Submitted Order");
 
-            bus.Start();
-
-            Console.WriteLine("Welcome to the Shop");
-            Console.WriteLine("Press Q key to exit");
-            Console.WriteLine("Press [0..9] key to order some products");
-            Console.WriteLine(string.Join(Environment.NewLine, Products.Select((x,i)=> $"[{i}]: {x.name} @ {x.price:C}")));
-
-            var products = new List<(string name, decimal price)>();
-            for (;;)
-            {
-                var consoleKeyInfo = Console.ReadKey(true);
-                if (consoleKeyInfo.Key == ConsoleKey.Q)
-                {
-                    break;
-                }
-
-
-                if (char.IsNumber(consoleKeyInfo.KeyChar))
-                {
-                    // Hack: I don't care about ½ etc...
-                    var product = Products[(int)char.GetNumericValue(consoleKeyInfo.KeyChar)];
-                    products.Add(product);
-                    Console.WriteLine($"Added {product.name}");
-                }
-
-                if (consoleKeyInfo.Key == ConsoleKey.Enter)
-                {
-                    bus.Publish<IOrderRequested>(new
-                    {
-                        Products = products.Select(x => new {Name = x.name, Price = x.price}).ToList()
-                    });
-
-                    Console.WriteLine("Submitted Order");
-
-                    products.Clear();
-                }
-            }
-
-            bus.Stop();
-        }
-
-
-        private static readonly IReadOnlyList<(string name, decimal price)> Products = new List<(string, decimal)>
-        {
-            ("Bread", 1.20m),
-            ("Milk", 0.50m),
-            ("Rice", 1m),
-            ("Pasta", 0.9m),
-            ("Pasta", 0.9m),
-            ("Cereals", 1.6m),
-            ("Chocolate", 2m),
-            ("Noodles", 1m),
-            ("Pie", 1m),
-            ("Sandwich", 1m),
-        };
+        products.Clear();
     }
 }
+
+bus.Stop();
+
